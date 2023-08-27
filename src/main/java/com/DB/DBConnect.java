8947e69edc56956684d51e6e147f88df3f19ede8
Rajesh <rajeshsahu7683995090@gmail.com>
2023-08-27 20:04:04 +0530

    datasource added

--- src/main/java/com/DB/DBConnect.java
+++ src/main/java/com/DB/DBConnect.java
@@ -9,19 +9,24 @@ import javax.sql.DataSource;
 public class DBConnect {
 	
 	public static Connection getsqlConnection() {
-		String dbSecrets = Dbsecret.getSecret();
-		String password = dbSecrets.split(",")[1].split(":")[1].replace("\"", "");
-		String username = password.substring(5);
-		String host = dbSecrets.split(",")[3].split(":")[1].replace("\"", "");
-		String port = dbSecrets.split(",")[4].split(":")[1].replace("\"", "");
-		Connection con = null;
+//		String dbSecrets = Dbsecret.getSecret();
+//		String password = dbSecrets.split(",")[1].split(":")[1].replace("\"", "");
+//		String username = password.substring(5);
+//		String host = dbSecrets.split(",")[3].split(":")[1].replace("\"", "");
+//		String port = dbSecrets.split(",")[4].split(":")[1].replace("\"", "");
+		
 		try {
-				con = DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/ebookaws", username,password);
+			InitialContext ic = new InitialContext();
+			DataSource ds = (DataSource) ic.lookup("java:/comp/env/MysqlDb");
+			//con = DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/ebookaws", username,password);
+			Connection con = ds.getConnection();
+			return con;
 				
 		} catch (Exception e) {
 			e.printStackTrace();
+			return null;
 		}
-		return con;
+		
 	}
 
 }
